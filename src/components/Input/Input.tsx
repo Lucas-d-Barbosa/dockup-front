0ae237765e2209e7c2c177e7a6dae74bd58cb37a
Lucas Barbosa <francisco.lucas.d.santos.barbosa@gmail.com>
2025-05-29 23:52:34 -0300

    fix: Correção do erro da propriedade InputProps

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,9 +1,11 @@
-import type { HTMLAttributes } from "react";
+import type { InputHTMLAttributes } from "react";
 import styles from "./Input.module.css";
-type InputProps =
-  | {
-      type: HTMLInputElement["type"];
-    } & HTMLAttributes<HTMLInputElement>;
-export function Input({ type, ...props }: InputProps) {
-  return <input type={type} {...props} className={styles.input}></input>;
+
+// Estende os atributos padrão e personaliza o tipo da prop "type"
+type InputProps = {
+  type: HTMLInputElement["type"];
+} & InputHTMLAttributes<HTMLInputElement>;
+
+export function Input({ type = "text", ...props }: InputProps) {
+  return <input type={type} {...props} className={styles.input} />;
 }
