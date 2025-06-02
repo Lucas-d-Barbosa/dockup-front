f89cef3fe62ec02df3d47f26c40e2a837e5d4623
Lucas Barbosa <francisco.lucas.d.santos.barbosa@gmail.com>
2025-06-02 00:47:37 -0300

    feat: Uso do UserContext para puxar o nome do usuário no header.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,14 @@
+import { FaUserCircle } from "react-icons/fa";
 import styles from "./Header.module.css";
+import { useContext } from "react";
+import UserContext from "../../contexts/UserContext/UserContext";
 export function Header() {
+  const user = useContext(UserContext);
+  const name = user?.name ?? "amigo";
   return (
     <header className={styles.header}>
-      <div className={styles.logo}></div>
+      <h2>{name}</h2>
+      <FaUserCircle style={{ marginRight: "3rem" }} size={50} />
     </header>
   );
 }
